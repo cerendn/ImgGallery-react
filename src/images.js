85f00bb57d8a907831f2b0c3c14c1dbcf72f8ae4
cerendn <cerendncer@outlook.com>
2024-04-19 16:59:19 +0300

    gallery

---
+++ src/images.js
@@ -0,0 +1,13 @@
+export default [
+  "https://i.nefisyemektarifleri.com/2023/01/10/aromaterapi-yaglari-faydalari-ve-kullanimi-17.jpg",
+
+  "https://i.nefisyemektarifleri.com/2023/01/10/aromaterapi-yaglari-faydalari-ve-kullanimi-1.jpg",
+
+  "https://www.aysetolga.com/wp-content/uploads/2017/01/portakal-yagi-3.jpg",
+
+  "https://www.uplifers.com/app/uploads/2018/01/esansiyel-yag.jpeg",
+
+  "https://img.baba-blog.com/2023/12/selection-of-essential-oils-with-herbs-and-flowers-in-the-background.jpeg?x-oss-process=style%2Ffull",
+
+  "https://i0.wp.com/vannajainfo.ru/wp-content/uploads/2017/12/Maslo-dlya-vannoy-12.jpg",
+];
